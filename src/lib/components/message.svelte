a36eb3e5ce3e005aa19b737eb1e8c2b9568d2530
SandeepDev1 <saisandeep1811@gmail.com>
2022-09-08 16:42:30 +0530

    message fix

--- src/lib/components/message.svelte
+++ src/lib/components/message.svelte
@@ -5,8 +5,8 @@
                 <div>
                     <h1 class="flex text-4xl text-right font-bold uppercase align-middle items-center justify-center pb-4 text-purpleS1">message from secretary</h1>
                 </div>
-                <div class="flex flex-col w-[70%]">
-                    <p class="text-left">
+                <div class="flex flex-col w-[80%]">
+                    <p class="text-justify">
                         With an ideology that knowledge is one value that never ever diminishes is what helped me and my husband establish BITS in 2008. Where education is all about managing human resources, I call BITS a women institution. For me those priceless satisfied smiles of parents and students are indeed a blessing. Coming up to the pillars of my entrepreneurial mind, I set my own code of conduct. Initially, the staff has to be paid on time which makes them feel safe and secure. I assure faculty of my institution are assumes of Health and Happiness.<br><br>
                         Born and brought up as a warrior princess, I was always respected like a leader from my family. For me, if not into this, I aspired of becoming a Doctor and run a mobile hospital van that could facilitate the poor. Coming to my family, my husband is a B.E and MBA degree holder turned politician. I’m blessed with two daughters – Amulya and Anishka.<br><br>
                         If asked upon my Success secret, I would undoubtedly say – I walk the talk, Enjoy the challenges and believe in transparency as well as hard work. I’m a kind of a woman who believes that the best way to crack a problem is to go through it and finish it off. With loads of dreams ahead to accomplish my dreams in accordance to BITS, I look forward to mold even more young minds and shape them as responsible citizens for the country.<br><br>
@@ -16,7 +16,7 @@
             </div>
             <div class="flex flex-col w-[30%]">
                 <div class="flex items-center justify-center">
-                    <img src="/mam.jpg" alt="" class="object-cover h-[700px] rounded-2xl" >
+                    <img src="/mam.jpg" alt="" class="object-cover h-[600px] rounded-2xl" >
                 </div>
                 <div><h2 class="flex text-3xl items-center justify-center pt-4 text-purpleS2"><b>Dr.K.Srilakshmi</b></h2></div>
             </div>
