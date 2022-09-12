9c265d50b5cdd51c070be20fa568b73f84261ff5
Rahul1342002 <trahul13402@gmail.com>
2022-09-12 20:36:43 +0530

    responsivness added to placement

--- src/lib/components/placement.svelte
+++ src/lib/components/placement.svelte
@@ -1,154 +1,148 @@
 <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet" />
 
-<section class="h-[40vh] flex ">
-  <div class="container  flex flex-row px-4 sm:px-12 py-8 w-1/2  justify-center items-center">
-    <div class="slider  ">
-      <div class="slide-track ">
-        <div class="slide "><img src="static\company\Amazon logo.png" alt="" /></div>
-        <div class="slide "><img src="static\company\Amazon logo.png" alt="" /></div>
-        <div class="slide "><img src="static\company\Amazon logo.png" alt="" /></div>
-        <div class="slide "><img src="static\company\Amazon logo.png" alt="" /></div>
-        <div class="slide "><img src="static\company\Amazon logo.png" alt="" /></div>
-        <div class="slide "><img src="static\company\Amazon logo.png" alt="" /></div>
-        <div class="slide "><img src="static\company\Amazon logo.png" alt="" /></div>
-        <div class="slide "><img src="static\company\Amazon logo.png" alt="" /></div>
-        <div class="slide "><img src="static\company\Amazon logo.png" alt="" /></div>
-        <div class="slide "><img src="static\company\Amazon logo.png" alt="" /></div>
-        <!-- same 9 slides doubled (duplicate) -->
-        <div class="slide "><img src="static\company\Amazon logo.png" alt="" /></div>
-        <div class="slide "><img src="static\company\Amazon logo.png" alt="" /></div>
-        <div class="slide "><img src="static\company\Amazon logo.png" alt="" /></div>
-        <div class="slide "><img src="static\company\Amazon logo.png" alt="" /></div>
-        <div class="slide "><img src="static\company\Amazon logo.png" alt="" /></div>
-        <div class="slide "><img src="static\company\Amazon logo.png" alt="" /></div>
-        <div class="slide "><img src="static\company\Amazon logo.png" alt="" /></div>
-        <div class="slide "><img src="static\company\Amazon logo.png" alt="" /></div>
-        <div class="slide "><img src="static\company\Amazon logo.png" alt="" /></div>
-        <div class="slide "><img src="static\company\Amazon logo.png" alt="" /></div>
-      </div>
-    </div>
-  </div>
+<section class="h-[40vh] lg:flex-row 3xs:flex-col flex ">
+	<div class="container  flex  px-4 sm:px-12 py-8 3xs:w-full lg:w-1/2 justify-center items-center basis-1/2">
+		<h3 class="text-center text-4xl ">COMPANY VISITED</h3>
+		<div class="slider  ">
+			<div class="slide-track ">
+				<div class="slide "><img src="/logo-150/Amazon.png" alt="" /></div>
+				<div class="slide "><img src="/logo-150/Amazon.png" alt="" /></div>
+				<div class="slide "><img src="/logo-150/Amazon.png" alt="" /></div>
+				<div class="slide "><img src="/logo-150/Amazon.png" alt="" /></div>
+				<div class="slide "><img src="/logo-150/Amazon.png" alt="" /></div>
+				<div class="slide "><img src="/logo-150/Amazon.png" alt="" /></div>
+				<div class="slide "><img src="/logo-150/Amazon.png" alt="" /></div>
+				<div class="slide "><img src="/logo-150/Amazon.png" alt="" /></div>
+				<div class="slide "><img src="/logo-150/Amazon.png" alt="" /></div>
+				<div class="slide "><img src="/logo-150/Amazon.png" alt="" /></div>
+				<!-- same 9 slides doubled (duplicate) -->
+				<div class="slide "><img src="/logo-150/Amazon.png" alt="" /></div>
+				<div class="slide "><img src="/logo-150/Amazon.png" alt="" /></div>
+				<div class="slide "><img src="/logo-150/Amazon.png" alt="" /></div>
+				<div class="slide "><img src="/logo-150/Amazon.png" alt="" /></div>
+				<div class="slide "><img src="/logo-150/Amazon.png" alt="" /></div>
+				<div class="slide "><img src="/logo-150/Amazon.png" alt="" /></div>
+				<div class="slide "><img src="/logo-150/Amazon.png" alt="" /></div>
+				<div class="slide "><img src="/logo-150/Amazon.png" alt="" /></div>
+				<div class="slide "><img src="/logo-150/Amazon.png" alt="" /></div>
+				<div class="slide "><img src="/logo-150/Amazon.png" alt="" /></div>
+			</div>
+		</div>
+	</div>
 
-  <div class="overflow-x-auto overflow-auto basis-1/2 h-[60vh] m-10">
-    <table class="table w-full">
-      <!-- head -->
-      <thead>
-        <tr>
-          <th />
-          <th>Name</th>
-          <th>Branch</th>
-          <th>Company</th>
-        </tr>
-      </thead>
-      <tbody>
-        <!-- row 1 -->
-        <tr>
-          <th>1</th>
-          <td>Cy Ganderton</td>
-          <td>Quality Control Specialist</td>
-          <td>Blue</td>
-        </tr>
-        <!-- row 2 -->
-        <tr>
-          <th>2</th>
-          <td>Hart Hagerty</td>
-          <td>Desktop Support Technician</td>
-          <td>Purple</td>
-        </tr>
-        <!-- row 3 -->
-        <tr>
-          <th>3</th>
-          <td>Brice Swyre</td>
-          <td>Tax Accountant</td>
-          <td>Red</td>
-        </tr>
-        <!-- row 4 -->
-        <tr>
-          <th>4</th>
-          <td>Brice Swyre</td>
-          <td>Tax Accountant</td>
-          <td>Red</td>
-        </tr>
-        <!-- row 5 -->
-        <tr>
-          <th>5</th>
-          <td>Brice Swyre</td>
-          <td>Tax Accountant</td>
-          <td>Red</td>
-        </tr>
-        <!-- row 6 -->
-        <tr>
-          <th>6</th>
-          <td>Brice Swyre</td>
-          <td>Tax Accountant</td>
-          <td>Red</td>
-        </tr>
-                  <!-- row 7 -->
-        <tr>
-          <th>7</th>
-          <td>Brice Swyre</td>
-          <td>Tax Accountant</td>
-          <td>Red</td>
-        </tr>
-                  <!-- row 8 -->
-        <tr>
-          <th>8</th>
-          <td>Brice Swyre</td>
-          <td>Tax Accountant</td>
-          <td>Red</td>
-        </tr>
-                  <!-- row 9 -->
-        <tr>
-          <th>9</th>
-          <td>Brice Swyre</td>
-          <td>Tax Accountant</td>
-          <td>Red</td>
-        </tr>
-                  <!-- row 10 -->
-        <tr>
-          <th>10</th>
-          <td>Brice Swyre</td>
-          <td>Tax Accountant</td>
-          <td>Red</td>
-        </tr>
-                  <!-- row 11 -->
-        <tr>
-          <th>11</th>
-          <td>Brice Swyre</td>
-          <td>Tax Accountant</td>
-          <td>Red</td>
-        </tr>
-                  <!-- row 12 -->
-        <tr>
-          <th>12</th>
-          <td>Brice Swyre</td>
-          <td>Tax Accountant</td>
-          <td>Red</td>
-        </tr>
-                  <!-- row 13 -->
-        <tr>
-          <th>13</th>
-          <td>Brice Swyre</td>
-          <td>Tax Accountant</td>
-          <td>Red</td>
-        </tr>
-                  <!-- row 14 -->
-        <tr>
-          <th>14</th>
-          <td>Brice Swyre</td>
-          <td>Tax Accountant</td>
-          <td>Red</td>
-        </tr>
-                  <!-- row 15 -->
-        <tr>
-          <th>15</th>
-          <td>Brice Swyre</td>
-          <td>Tax Accountant</td>
-          <td>Red</td>
-        </tr>
-
-      </tbody>
-    </table>
-  </div>
-  
+	<div class="overflow-x-auto overflow-auto m-10 basis-1/2">
+		<h3 class="text-center p-4 text-3xl">Placement Info</h3>
+		<table class="table w-full">
+			<!-- head -->
+			<thead>
+				<tr>
+					<th />
+					<th>Name</th>
+					<th>Job</th>
+					<th>Favorite Color</th>
+				</tr>
+			</thead>
+			<tbody>
+				<!-- row 1 -->
+				<tr>
+					<th>1</th>
+					<td>Cy Ganderton</td>
+					<td>Quality Control Specialist</td>
+					<td>Blue</td>
+				</tr>
+				<!-- row 2 -->
+				<tr>
+					<th>2</th>
+					<td>Hart Hagerty</td>
+					<td>Desktop Support Technician</td>
+					<td>Purple</td>
+				</tr>
+				<!-- row 3 -->
+				<tr>
+					<th>3</th>
+					<td>Brice Swyre</td>
+					<td>Tax Accountant</td>
+					<td>Red</td>
+				</tr>
+				<tr>
+					<th>3</th>
+					<td>Brice Swyre</td>
+					<td>Tax Accountant</td>
+					<td>Red</td>
+				</tr>
+				<tr>
+					<th>3</th>
+					<td>Brice Swyre</td>
+					<td>Tax Accountant</td>
+					<td>Red</td>
+				</tr>
+				<tr>
+					<th>3</th>
+					<td>Brice Swyre</td>
+					<td>Tax Accountant</td>
+					<td>Red</td>
+				</tr>
+				<tr>
+					<th>3</th>
+					<td>Brice Swyre</td>
+					<td>Tax Accountant</td>
+					<td>Red</td>
+				</tr>
+				<tr>
+					<th>3</th>
+					<td>Brice Swyre</td>
+					<td>Tax Accountant</td>
+					<td>Red</td>
+				</tr>
+				<tr>
+					<th>3</th>
+					<td>Brice Swyre</td>
+					<td>Tax Accountant</td>
+					<td>Red</td>
+				</tr>
+				<tr>
+					<th>3</th>
+					<td>Brice Swyre</td>
+					<td>Tax Accountant</td>
+					<td>Red</td>
+				</tr>
+				<tr>
+					<th>3</th>
+					<td>Brice Swyre</td>
+					<td>Tax Accountant</td>
+					<td>Red</td>
+				</tr>
+				<tr>
+					<th>3</th>
+					<td>Brice Swyre</td>
+					<td>Tax Accountant</td>
+					<td>Red</td>
+				</tr>
+				<tr>
+					<th>3</th>
+					<td>Brice Swyre</td>
+					<td>Tax Accountant</td>
+					<td>Red</td>
+				</tr>
+				<tr>
+					<th>3</th>
+					<td>Brice Swyre</td>
+					<td>Tax Accountant</td>
+					<td>Red</td>
+				</tr>
+				<tr>
+					<th>3</th>
+					<td>Brice Swyre</td>
+					<td>Tax Accountant</td>
+					<td>Red</td>
+				</tr>
+				<tr>
+					<th>3</th>
+					<td>Brice Swyre</td>
+					<td>Tax Accountant</td>
+					<td>Red</td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
 </section>
