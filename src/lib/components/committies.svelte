af2f511cbe12ad8417b349812a17214a639f1bcb
Rahul1342002 <trahul13402@gmail.com>
2022-09-14 16:13:10 +0530

    divider fixed

--- src/lib/components/committies.svelte
+++ src/lib/components/committies.svelte
@@ -58,15 +58,15 @@
     ]
 </script>
 
-<section class="text-center bg-slate-800 font-sans xs:px-0 3xs:p-24">
-    <div class="flex flex-col justify-center items-center">
-        <div class="flex flex-col items-center">
+<section class="text-center bg-slate-800 font-sans xs:px-0 3xs:p-24 ">
+    <div class="flex flex-col justify-center items-center ">
+        <div class="flex flex-col items-center ">
             <p class=" text-3xl text-white p-3">Professional Committie</p>
             <p class=" text-2xl text-white pb-2">BITS Vizag provides you </p>
         </div>
         <div class="grid xl:grid-cols-5 lg:grid-cols-4 gap-2 md:grid-cols-2 xs:grid-cols-2   items-center ">
             {#each items as item, i}
-                <div on:mouseenter={() => scaleUp(".card" + i.toString())} on:mouseleave={() => {scaleDown(".card" + i.toString())}} class={`card${i} h-[150px] w-[200px] bg-green-300 flex flex-col items-center m-2 rounded-[5px]`}>
+                <div on:mouseenter={() => scaleUp(".card" + i.toString())} on:mouseleave={() => {scaleDown(".card" + i.toString())}} class={`card${i} h-[150px] w-[200px] bg-green-300 flex flex-col items-center m-2 rounded-[5px] `}>
                     <div class="p-[10px] capitalize font-bold">{item.title}</div>
                     <div class="capitalize overflow-hidden text-center">{item.description}</div>
                 </div>
