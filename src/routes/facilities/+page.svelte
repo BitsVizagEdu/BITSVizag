b84710274b1d76226fe04e7a62640f7775d1d509
ANCIENTINSANE <surendrakumar246810.bits@gmail.com>
2022-09-15 12:10:49 +0530

    added facilities

---
+++ src/routes/facilities/+page.svelte
@@ -0,0 +1,68 @@
+<script>
+     let activeTab = 'Library';
+    
+
+	let items = ['Library','Hostel','Cafeteria','Sports','Laboratory','Transport']
+	
+
+    import Library from './components/library.svelte';
+
+	import Sports from "./components/sports.svelte";
+    
+    import Laboratory from './components/laboratory.svelte';
+    
+	import Cafeteria from './components/cafeteria.svelte';
+
+	import Hostel from './components/hostel.svelte';
+
+	import Transport from './components/transport.svelte'
+
+    import Footer from '$lib/components/footer.svelte';
+	
+
+</script>
+
+<svelte:head>
+	<title>About Us</title>
+</svelte:head>
+
+
+	<div class="total flex lg:flex-row 3xs:flex-col  h-[100%] ">
+		<aside class="lg:w-[25%] p-5 rounded-lg  " aria-label="Sidebar">
+			<div class="overflow-y-auto py-4 px-3   bg-blue-900 rounded-lg">
+				<ul class="space-y-2">
+					{#each items as item}
+					<li  id="{item}" on:click={() => (activeTab = item)}>
+						<a href="#" class="flex items-center lg:justify-center p-2 text-base font-normal rounded-lg  
+								{(activeTab === item) ? ' bg-white text-black ' : 'text-white'} ">
+								<span class="ml-3 md:text-base 2xl:text-center  3xs:text-xs xs:text-sm">{item}</span>
+							</a>
+						</li>
+					{/each}
+				</ul>
+			</div>
+		</aside>
+
+		<div class=" lg:w-[75%] p-5 rounded-lg ">
+			{#if activeTab === 'Library'}
+				<Library/>
+			{/if}
+			{#if activeTab === 'Sports'}
+				<Sports/>
+			{/if}
+			{#if activeTab === 'Laboratory'}
+				<Laboratory/>
+			{/if}
+			{#if activeTab === 'Cafeteria'}
+				<Cafeteria/>
+			{/if}
+			{#if activeTab === 'Hostel'}
+				<Hostel/>
+			{/if}
+			{#if activeTab === 'Transport'}
+				<Transport/>
+			{/if}
+			</div>
+	</div>
+	<Footer/>
+
---
+++ src/routes/facilities/components/library.svelte
@@ -0,0 +1,39 @@
+<section class=" bg-slate-200 3xs:p-4  md:p-10 rounded-lg">
+    <div class="flex  flex-col-reverse justify-center items-center  mx-auto   ">
+        <div id="text" class=" lg:w-[90%] 3xs:mb-3 lg:px-10">
+            <div class="text-red1 3xs:text-center lg:text-center xs:text-[20px] sm:text-[25px] font-bold 3xs:pb-1 md:pb-6">
+                <h2>LIBRARY</h2>
+            </div>
+            <div>
+                <p class="text-justify md:text-[15px]  3xs:tracking-normal lg:text-[20px] xs:tracking-widest mt-4  md:font-normal lg:leading-6 my-1 3xs:font-light xs:text-sm  tracking-wide py-1">
+                    The Library of BITS Vizag was developed to keep up Standards. The library is spacious and well-protected with security system. Specialized collections of Books, Journals & Non-book materials are available in Engineering & Technology, Basic Sciences, and Management Sciences. It is replete with 20,000 volumes of books, more than 500 Journal Back Volumes.
+                </p>
+            </div>
+            <div>
+                 <p class="text-justify md:text-[15px] 3xs:tracking-normal lg:text-[20px] xs:tracking-widest mt-4  md:font-normal lg:leading-6 my-1 3xs:font-light xs:text-sm tracking-wide py-1">
+                    The Library contributes to the fulfillment of the Institution’s mission by selecting, acquiring, organizing, maintaining and making accessible a collection of printed and non-printed, primary and secondary materials that will support the educational, research programs to the fullest.
+                </p>
+            </div>
+            <div>
+                 <p class="text-justify md:text-[15px] 3xs:tracking-normal lg:text-[20px] xs:tracking-widest mt-4  md:font-normal lg:leading-6 my-1 3xs:font-light xs:text-sm tracking-wide py-1">
+                    The library is spacious and well-protected with security system with specialized collections of Books, Journals & Non-book materials in all areas.<br>
+                    Providing library users with point-of-use instruction, personal assistance in conducting literature research and other reference services.                    
+                </p>
+            </div>
+            <div>
+                <p class="text-justify md:text-[15px] 3xs:tracking-normal lg:text-[20px] xs:tracking-widest mt-4  md:font-normal lg:leading-6 my-1 3xs:font-light xs:text-sm tracking-wide py-1">
+                    Providing an environment conducive to the optimum use of library materials and an appropriate schedule of hours of service and professional assistance.<br>
+                    Participating in overall computing resources plan and providing for full library utilization of automation technology, physical facilities and equipment adequate to process, catalogue and store the materials.                    
+                 </p>
+           </div>
+
+
+        </div>
+        <div id="pic " class="flex flex-col justify-between ">
+            <div class="rounded-lg"><img src="./Library.jpg" class=" 3xs:w-full xs:w-full md:h-[320px] lg:h-[420px] 3xs:p-1  md:p-3">
+            </div>
+            
+            
+        </div>
+    </div>
+</section>
