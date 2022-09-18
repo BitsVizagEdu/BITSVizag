81ddabc5c50891a0a8863e097f9e2db8a3ac6255
Rahul1342002 <trahul13402@gmail.com>
2022-09-18 21:36:21 +0530

    exam-cell template created

---
+++ src/routes/examcell/Engineering/+page.svelte
@@ -0,0 +1,73 @@
+<script>
+     let activeTab = 'Library';
+    
+
+	let items = ['Academic Calendar','Academic Regulation','Syllabus','Results','Timetable','Circular & Notification','Online Discrepancies','Department Examcell Coordinators','Contactus']
+	
+	import Calendar from "./components/calendar.svelte";
+	import Regulation from "./components/regulation.svelte";
+	import Results from "./components/results.svelte";
+	import Timetable from "./components/timetable.svelte";
+	import Footer from "$lib/components/footer.svelte";
+	import Circular from "./components/circular.svelte";
+	import Online from "./components/online.svelte";
+    import Coordinators from "./components/coordinators.svelte";
+	import Contactus from "./components/contactus.svelte";
+	import Syllabus from "./components/syllabus.svelte";
+	
+
+</script>
+
+<svelte:head>
+	<title>About Us</title>
+</svelte:head>
+
+
+	<div class="total flex lg:flex-row 3xs:flex-col  h-[100%] ">
+		<aside class="lg:w-[25%] p-5 rounded-lg  " aria-label="Sidebar">
+			<div class="overflow-y-auto py-4 px-3   bg-blue-900 rounded-lg">
+				<ul class="space-y-2">
+					{#each items as item}
+					<li  id="{item}" on:click={() => (activeTab = item)}>
+						<a href="#" class="flex items-center lg:justify-center p-2 text-base font-normal rounded-lg  
+								{(activeTab === item) ? ' bg-white text-black ' : 'text-white'} ">
+								<span class="ml-3 md:text-base 2xl:text-center  3xs:text-xs xs:text-sm">{item}</span>
+							</a>
+						</li>
+					{/each}
+				</ul>
+			</div>
+		</aside>
+
+		<div class=" lg:w-[75%] p-5 rounded-lg ">
+			{#if activeTab === 'Academic Calendar'}
+				<Calendar/>
+			{/if}
+			{#if activeTab === 'Academic Regulation'}
+				<Regulation/>
+			{/if}
+			{#if activeTab === 'Syllabus'}
+				<Syllabus/>
+			{/if}
+			{#if activeTab === 'Results'}
+				<Results/>
+			{/if}
+			{#if activeTab === 'Timetable'}
+				<Timetable/>
+			{/if}
+			{#if activeTab === 'Circular & Notification'}
+				<Circular/>
+			{/if}
+			{#if activeTab === 'Online Discrepancies'}
+				<Online/>
+			{/if}
+			{#if activeTab === 'Department Examcell Coordinators'}
+				<Coordinators/>
+			{/if}
+			{#if activeTab === 'Contactus'}
+				<Contactus/>
+			{/if}
+			</div>
+	</div>
+	<Footer/>
+
---
+++ src/routes/examcell/MBA/components/calendar.svelte
@@ -0,0 +1,31 @@
+<section class=" bg-slate-200 3xs:p-4  md:p-10 rounded-lg">
+    <div class="flex  flex-col-reverse justify-center items-center  mx-auto   ">
+        <div id="text" class=" lg:w-[90%] 3xs:mb-3 lg:px-10">
+            <div class="text-red1 3xs:text-center lg:text-center xs:text-[20px] sm:text-[25px] font-bold 3xs:pb-1 md:pb-6">
+                <h2>ACADEMIC CALENDAR</h2>
+            </div>
+            <div>
+                <p class="text-justify md:text-[15px]  3xs:tracking-normal lg:text-[20px] xs:tracking-widest mt-4  md:font-normal lg:leading-6 my-1 3xs:font-light xs:text-sm  tracking-wide py-1">
+                    BITS Vizag has the provision of a spacious and hygienic canteen that caters to the taste of all students. 
+                </p>
+            </div>
+            <div>
+                 <p class="text-justify md:text-[15px] 3xs:tracking-normal lg:text-[20px] xs:tracking-widest mt-4  md:font-normal lg:leading-6 my-1 3xs:font-light xs:text-sm tracking-wide py-1">
+                    The canteen and cafeteria are equipped with modern furniture, cooking and storing and refrigeration facilities, which provide hot lunch, snacks and beverages both to the students and staff at reasonable rates.
+                </p>
+            </div>
+            <div>
+                 <p class="text-justify md:text-[15px] 3xs:tracking-normal lg:text-[20px] xs:tracking-widest mt-4  md:font-normal lg:leading-6 my-1 3xs:font-light xs:text-sm tracking-wide py-1">
+                    It can comfortably accommodate more than 300 students at a time.It is well maintained with efficient service counter..
+                </p>
+            </div>
+            <div>
+                <p class="text-justify md:text-[15px] 3xs:tracking-normal lg:text-[20px] xs:tracking-widest mt-4  md:font-normal lg:leading-6 my-1 3xs:font-light xs:text-sm tracking-wide py-1">
+                    The canteen serves a variety of dishes and remains open on all working days.Beverages as per the prevailing season and requirements of the students are available.To break away from the daily academic routine students can unwind during the evenings at cafeteria offering a wide range of fast food varieties at nominal prices.
+                 </p>
+           </div>
+
+        </div>
+        
+    </div>
+</section>
