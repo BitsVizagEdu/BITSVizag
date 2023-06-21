0b5293f3a9e3898b849e7aa68b9cd2c52a6adc57
Rahul1342002 <trahul13402@gmail.com>
2023-06-21 12:19:16 +0530

    logo and pg page added

---
+++ src/routes/courses/[slug]/components/Under Graduation.svelte
@@ -0,0 +1,161 @@
+<section class=" bg-slate-200 3xs:p-4  md:p-10 rounded-lg">
+	<h1 class="text-4xl font-bold text-center mb-10 text-red1">Under Graduation Programme</h1>
+	<div class="flex">
+		<div class="w-[50%] pr-7">
+			<h1 class="text-2xl xl:text-3xl font-bold ">Bachelor of Technology</h1>
+			<br />
+			<h1 class="text-justify ">
+				Bachelor of Technology (B.Tech): Embark on a transformative journey in the field of
+				technology with our B.Tech program. Choose from specialized streams such as Computer
+				Science, Electronics and Communication Engineering, Electrical and Electronic Engineering,
+				Mechanical Engineering, and Civil Engineering. Our rigorous curriculum, combined with
+				hands-on practical experience, will prepare you to excel in the ever-evolving world of
+				technology.
+			</h1>
+			<br />
+			<h1 class="text-justify">
+				BITS Engineering College admits candidates through the Engineering, Agriculture & Pharmacy
+				Common Entrance Test (EAPCET), conducted by AP STATE GOVERNMENT, on fulfilling the
+				eligibility requirements and under management quota
+			</h1>
+		</div>
+
+		<div class="w-[50%]">
+			<img src="/ug.jpg" alt="" />
+		</div>
+	</div>
+	<div>
+		<h1 class="text-2xl">Courses Offered</h1>
+	</div>
+
+    <br>
+    <!-- ====== Table Section Start -->
+<section class="bg-white ">
+    <div class="container mx-auto">
+      <div class="-mx-4 flex flex-wrap">
+        <div class="w-full px-4">
+          <div class="max-w-full overflow-x-auto">
+            <table class="w-full table-auto">
+              <thead>
+                <tr class="bg-red1 bg-opacity-90 text-center">
+                  <th
+                    class="w-1/3 min-w-[160px] border-l border-transparent py-4 px-3 text-lg font-semibold  lg:py-7 lg:px-4 text-white"
+                  >
+                    Sl.No
+                  </th>
+                  <th
+                    class="w-1/3 min-w-[160px] py-4 px-3 text-lg font-semibold  lg:py-7 lg:px-4 text-white"
+                  >
+                    Courses Offered
+                  </th>
+                  <th
+                    class="w-1/3 min-w-[160px] py-4 px-3 text-lg font-semibold  lg:py-7 lg:px-4 text-white"
+                  >
+                   Intake
+                  </th>
+                 
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td
+                    class="text-dark border-b border-l border-[#E8E8E8] bg-[#F3F6FF] py-5 px-2 text-center text-base font-medium"
+                  >
+                    1
+                  </td>
+                  <td
+                    class="text-dark border-b border-[#E8E8E8] bg-white py-5 px-2 text-center text-base font-medium"
+                  >
+                    Civil Engineering
+                  </td>
+                  <td
+                    class="text-dark border-b border-[#E8E8E8] bg-[#F3F6FF] py-5 px-2 text-center text-base font-medium"
+                  >
+                    60
+                  </td>
+                  
+                </tr>
+                <tr>
+                  <td
+                    class="text-dark border-b border-l border-[#E8E8E8] bg-[#F3F6FF] py-5 px-2 text-center text-base font-medium"
+                  >
+                    2
+                  </td>
+                  <td
+                    class="text-dark border-b border-[#E8E8E8] bg-white py-5 px-2 text-center text-base font-medium"
+                  >
+                  Electrical and Electronic Engineering
+                  </td>
+                  <td
+                    class="text-dark border-b border-[#E8E8E8] bg-[#F3F6FF] py-5 px-2 text-center text-base font-medium"
+                  >
+                    60
+                  </td>
+                  
+                </tr>
+                <tr>
+                  <td
+                    class="text-dark border-b border-l border-[#E8E8E8] bg-[#F3F6FF] py-5 px-2 text-center text-base font-medium"
+                  >
+                    3
+                  </td>
+                  <td
+                    class="text-dark border-b border-[#E8E8E8] bg-white py-5 px-2 text-center text-base font-medium"
+                  >
+                  Electronics and Communication Engineering
+                  </td>
+                  <td
+                    class="text-dark border-b border-[#E8E8E8] bg-[#F3F6FF] py-5 px-2 text-center text-base font-medium"
+                  >
+                    120
+                  </td>
+                 
+                </tr>
+
+                <tr>
+                    <td
+                      class="text-dark border-b border-l border-[#E8E8E8] bg-[#F3F6FF] py-5 px-2 text-center text-base font-medium"
+                    >
+                      3
+                    </td>
+                    <td
+                      class="text-dark border-b border-[#E8E8E8] bg-white py-5 px-2 text-center text-base font-medium"
+                    >
+                    Mechanical Engineering
+                    </td>
+                    <td
+                      class="text-dark border-b border-[#E8E8E8] bg-[#F3F6FF] py-5 px-2 text-center text-base font-medium"
+                    >
+                     60
+                    </td>
+                   
+                  </tr>
+
+                  <tr>
+                    <td
+                      class="text-dark border-b border-l border-[#E8E8E8] bg-[#F3F6FF] py-5 px-2 text-center text-base font-medium"
+                    >
+                      3
+                    </td>
+                    <td
+                      class="text-dark border-b border-[#E8E8E8] bg-white py-5 px-2 text-center text-base font-medium"
+                    >
+                    Computer Science Engineering
+                    </td>
+                    <td
+                      class="text-dark border-b border-[#E8E8E8] bg-[#F3F6FF] py-5 px-2 text-center text-base font-medium"
+                    >
+                      180
+                    </td>
+                   
+                  </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- ====== Table Section End -->
+  
+</section>
---
+++ src/routes/courses/[slug]/components/utils.js
@@ -0,0 +1,5 @@
+export let items = ['Under Graduation','Post Graduation']
+
+export function replaceHyphenWithSpace(text) {
+    return text.replace(/-/g, " ")
+}
