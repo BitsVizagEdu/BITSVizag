f371690206ddbd5755ef9584027e598b89fbf946
Chaitanya <Chaitanyadwarapudi123@gmail.com>
2023-07-19 16:46:07 +0530

    added the new elements

--- src/routes/aboutus/[slug]/components/principal.svelte
+++ src/routes/aboutus/[slug]/components/principal.svelte
@@ -27,7 +27,7 @@
         </div>
         <div id="pic " class="flex flex-col justify-between ">
             <div>
-                <img src="/principal.jpg" class=" 3xs:h-[300px] xs:h-[400px] md:h-[550px] p-3 rounded-md xl:h-auto" alt=""/>
+                <img src="/principal.jpg" class=" 3xs:h-[300px] xs:h-[400px] md:h-[550px] p-3 rounded-[50px] xl:h-auto" alt=""/>
             </div>
             <div><p class=" text-center font-bold text-2xl" >Dr. Govinda Raju Matha</p>
             </div>
