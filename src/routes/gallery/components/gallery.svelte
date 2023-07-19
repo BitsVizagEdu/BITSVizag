a1051176b4dd11101004ce07e062280ab6d1ca2d
Rahul1342002 <trahul13402@gmail.com>
2023-07-19 15:15:42 +0530

    placement update

--- src/routes/gallery/components/gallery.svelte
+++ src/routes/gallery/components/gallery.svelte
@@ -1,5 +1,21 @@
 <section>
     <div class="grid grid-cols-4 grid-rows-3">
+        <div  class="col-span-2 row-span-2 col-start-1 col-end-3 row-start-1 row-end-3"><img src="/maingall/img1.jpeg" alt="" class="aspect-square"></div>
+        <div class="col-span-1 row-span-1 col-start-3 col-end-4 row-start-1 row-end-3"><img src="/gallery/img1.jpeg" alt="" class="aspect-square"></div>
+        <div class=" col-span-1 row-span-1 col-start-4 col-end-5 row-start-1 row-end-3"><img src="/gallery/img3.jpeg" alt="" class="aspect-square"></div>
+        <div class="col-span-2 row-span-2 col-start-3 col-end-5 row-start-2 row-end-4"><img src="/gallery/img6.jpeg" alt="" class="aspect-square"></div>
+        <div class="col-span-1 row-span-1 col-start-1 col-end-2 row-start-3 row-end-4"><img src="/gallery/img4.jpeg" alt="" class="aspect-square"></div>
+        <div class="col-span-1 row-span-1 col-start-2 col-end-3 row-start-3 row-end-4"><img src="/gallery/img5.jpeg" alt="" class="aspect-square"></div>
+    </div>
+    <div class="grid grid-cols-4 grid-rows-3">
+        <div  class="col-span-2 row-span-2 col-start-1 col-end-3 row-start-1 row-end-3"><img src="/gallery/img2.jpeg" alt="" class="aspect-square"></div>
+        <div class="col-span-1 row-span-1 col-start-3 col-end-4 row-start-1 row-end-3"><img src="/gallery/img1.jpeg" alt="" class="aspect-square"></div>
+        <div class=" col-span-1 row-span-1 col-start-4 col-end-5 row-start-1 row-end-3"><img src="/gallery/img3.jpeg" alt="" class="aspect-square"></div>
+        <div class="col-span-2 row-span-2 col-start-3 col-end-5 row-start-2 row-end-4"><img src="/gallery/img6.jpeg" alt="" class="aspect-square"></div>
+        <div class="col-span-1 row-span-1 col-start-1 col-end-2 row-start-3 row-end-4"><img src="/gallery/img4.jpeg" alt="" class="aspect-square"></div>
+        <div class="col-span-1 row-span-1 col-start-2 col-end-3 row-start-3 row-end-4"><img src="/gallery/img5.jpeg" alt="" class="aspect-square"></div>
+    </div>
+    <div class="grid grid-cols-4 grid-rows-3">
         <div  class="col-span-2 row-span-2 col-start-1 col-end-3 row-start-1 row-end-3"><img src="/gallery/img2.jpeg" alt="" class="aspect-square"></div>
         <div class="col-span-1 row-span-1 col-start-3 col-end-4 row-start-1 row-end-3"><img src="/gallery/img1.jpeg" alt="" class="aspect-square"></div>
         <div class=" col-span-1 row-span-1 col-start-4 col-end-5 row-start-1 row-end-3"><img src="/gallery/img3.jpeg" alt="" class="aspect-square"></div>
