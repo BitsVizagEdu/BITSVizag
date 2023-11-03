336eb28ae61a0202bf0a3e5295ac086a96213786
Chaitanya <Chaitanyadwarapudi123@gmail.com>
2023-11-03 15:38:38 +0530

    added the alumni

--- src/routes/alumini-data/components/alumini.svelte
+++ src/routes/alumini-data/components/alumini.svelte
@@ -1,3 +1,8 @@
+
+<div class="text-center text-7xl text-[#1f2937] font-bold p-10">
+  <h1>ALUMNI</h1>
+</div>
+
 <div class="container mx-auto px-5 py-2 lg:px-32 ">
     <div class="-m-1 flex flex-wrap md:-m-2">
       <div class="flex w-1/2 flex-wrap">
@@ -64,7 +69,7 @@
   <section>
     <div class="flex lg:flex-row 3xs:flex-col justify-center justify-around bg-[#d22626] md:p-16 3xs:p-10 md:m-28 md:mt-10 items-center rounded-3xl">
       <div class="text-white">
-        <h1 class="md:text-5xl 3xs:text-4xl font-bold">Be a Part of BIG BITS Family</h1>
+        <h1 class="md:text-5xl 3xs:text-4xl font-bold">Be a Part of BIG BITS Family !</h1>
         <p class="md:text-2xl leading-10">If you are a Bits Aulmni Register the Form</p>
       </div>
       <div class="items-center">
