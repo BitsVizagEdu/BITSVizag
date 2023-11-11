db948c4588b70407bf7dff2d3a086f900b0a351a
Rahul1342002 <trahul13402@gmail.com>
2023-11-11 12:12:45 +0530

    header updated - T_HUB

--- src/lib/components/placementheader.svelte
+++ src/lib/components/placementheader.svelte
@@ -1,4 +1,4 @@
-<h1 class="3xs:text-sm xs:text-xl md:text-3xl text-center p-4 bg-white ">BITS Vizag signs a Prestigious Global MOU with "The University of Memphis", Tennessee at the University Campus, United States on 7th Sep, 2023</h1>
-<a href="/placements"><img src="/header/MOU-Memphis.jpg" alt=""></a>
+<h1 class="3xs:text-sm xs:text-xl md:text-3xl text-center p-4 bg-white ">Students of BITS Vizag visited T-HUB, Hyderabad for Industrial Tour  on 8th Nov, 2023</h1>
+<a href="/placements"><img src="/header/thub.png" alt=""></a>
 <h1 class="3xs:text-sm xs:text-xl md:text-3xl text-center p-4 bg-white ">THE TRAINING AND PLACEMENT CELL IS EXCITED TO ANNOUNCE <br>THE HIGHEST PACKAGE OF CLASS 2023</h1>
 <a href="/placements"><img src="/header/Sandeep.jpg" alt=""></a>
