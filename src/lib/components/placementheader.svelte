1f18aedc9eca7fad180644623b6086a52db95045
Rahul1342002 <trahul13402@gmail.com>
2023-11-14 10:15:25 +0530

    link updated T-HUB

--- src/lib/components/placementheader.svelte
+++ src/lib/components/placementheader.svelte
@@ -1,4 +1,4 @@
 <h1 class="3xs:text-sm xs:text-xl md:text-3xl text-center p-4 bg-white ">Students of BITS Vizag visited T-HUB, Hyderabad for Industrial Tour  on 8th Nov, 2023</h1>
-<a href="/placements"><img src="/header/thub.png" alt=""></a>
+<a href="https://www.instagram.com/p/CzbRQZFr_rq/?utm_source=ig_web_copy_link&igshid=MzRlODBiNWFlZA=="><img src="/header/thub.png" alt=""></a>
 <h1 class="3xs:text-sm xs:text-xl md:text-3xl text-center p-4 bg-white ">THE TRAINING AND PLACEMENT CELL IS EXCITED TO ANNOUNCE <br>THE HIGHEST PACKAGE OF CLASS 2023</h1>
 <a href="/placements"><img src="/header/Sandeep.jpg" alt=""></a>
