d3b04d1e59faa80cc1a2fda0045a12d3e43f8767
Chaitanya <Chaitanyadwarapudi123@gmail.com>
2024-02-15 13:35:25 +0530

    added the elements in mous

--- src/routes/placements/component/coordinates-student.svelte
+++ src/routes/placements/component/coordinates-student.svelte
@@ -1,88 +1,66 @@
 <script>
+	// import {onMount} from "svelte";
+	//
+	// onMount(async () => {
+	//
+	// })
+	import { animate } from 'motion';
+	function scaleUp(element) {
+		animate(document.querySelector(element), { scale: 1.2 }, { duration: 0.3 });
+	}
 
-    // import {onMount} from "svelte";
-    //
-    // onMount(async () => {
-    //
-    // })
-    import {animate} from "motion";
-    function scaleUp(element) {
-        animate(document.querySelector(element), {scale: 1.2}, {duration: 0.3})
-    }
+	function scaleDown(element) {
+		animate(element, { scale: 1 }, { duration: 0.3 });
+	}
 
-    function scaleDown(element) {
-        animate(element, {scale: 1}, {duration: 0.3})
-    }
-
-    let items = [
-        {
-            title: "Mr. Sandeep",
-            description: "Computer Science & Engg",
-        },
-        {
-            title: "Mrs. Rahul",
-            description: "Electronics & Communication Engg",
-        },
-        {
-            title: "Mr. Chaitanya",
-            description: "Electrical & Electronics Engg"
-        },
-        {
-            title: "Mr. Dan Alfred",
-            description: "Mechanical Engg",
-        },
-        {
-            title: "Mr. Sai Kiran",
-            description: "Civil Engg"
-        },
-        {
-            title: "Mrs. Aditya",
-            description: "Management Studies"
-        },
-        {
-            title: "Mrs. Aditya",
-            description: "Management Studies"
-        },
-        {
-            title: "Mrs. Aditya",
-            description: "Management Studies"
-        },
-        {
-            title: "Mrs. Aditya",
-            description: "Management Studies"
-        },
-        {
-            title: "Mrs. Aditya",
-            description: "Management Studies"
-        },
-        {
-            title: "Mrs. Aditya",
-            description: "Management Studies"
-        },
-        {
-            title: "Mrs. Aditya",
-            description: "Management Studies"
-        },
-        
-    ]
+	let items = [
+		{
+			title: 'Mr. Sandeep',
+			description: 'Computer Science & Engg'
+		},
+		{
+			title: 'Mr. Rahul',
+			description: 'Computer Science & Engg'
+		},
+		{
+			title: 'Mr. Chaitanya',
+			description: 'Computer Science & Engg'
+		},
+		{
+			title: 'Mr. Pavan Varma',
+			description: 'Computer Science & Engg'
+		},
+		{
+			title: 'Mr. Aditya',
+			description: 'Computer Science & Engg'
+		},
+		{
+			title: 'Mr. Sai Kiran',
+			description: 'Computer Science & Engg'
+		}
+	];
 </script>
 
 <section class="text-center  font-sans xs:px-0 3xs:p-11  ">
-    <div class="flex flex-col justify-center items-center ">
-        <div class="flex flex-col items-center ">
-            <p class=" text-4xl font-bold pb-10">Student Co-Ordinators</p>
-            
-        </div>
-        <div class="grid xl:grid-cols-4 lg:grid-cols-3 gap-2 md:grid-cols-3 xs:grid-cols-1  ">
-            
-            {#each items as item, i}
-                <div>
-                    <a href={item.link} on:mouseleave={() => {scaleDown(".card" + i.toString())}} class={`card${i} h-[200px] w-[225px]  lg:w-[300px]  bg-white flex flex-col justify-center shadow-[rgba(6,_24,_44,_0.4)_0px_0px_0px_2px,_rgba(6,_24,_44,_0.65)_0px_4px_6px_-1px,_rgba(255,_255,_255,_0.08)_0px_1px_0px_inset] items-center  m-2 rounded-[5px] `}>
-                        <div class="p-[10px] text-center capitalize font-bold text-xl">{item.title}</div>
-                        <div class="p-[10px] text-center capitalize text-xl">{item.description}</div>
-                    </a>
-                </div>
-            {/each}
-        </div>
-    </div>
+	<div class="flex flex-col justify-center items-center ">
+		<div class="flex flex-col items-center ">
+			<p class=" text-4xl font-bold pb-10">Student Coordinators</p>
+		</div>
+		<div class="grid xl:grid-cols-3 lg:grid-cols-3 gap-2 md:grid-cols-3 xs:grid-cols-1  ">
+			{#each items as item, i}
+				<div>
+					<a
+						href={item.link}
+						on:mouseleave={() => {
+							scaleDown('.card' + i.toString());
+						}}
+						class={`card${i} h-[200px] w-[225px]  lg:w-[300px]  bg-white flex flex-col justify-center shadow-[rgba(6,_24,_44,_0.4)_0px_0px_0px_2px,_rgba(6,_24,_44,_0.65)_0px_4px_6px_-1px,_rgba(255,_255,_255,_0.08)_0px_1px_0px_inset] items-center  m-2 rounded-[5px] `}
+					>
+						<div class="p-[10px] text-center capitalize font-bold text-xl">{item.title}</div>
+						<div class="p-[10px] text-center capitalize text-xl">{item.description}</div>
+					</a>
+				</div>
+			{/each}
+		</div>
+	</div>
 </section>
--- src/routes/placements/component/mous.svelte
+++ src/routes/placements/component/mous.svelte
@@ -1,67 +1,78 @@
-
 <section>
-    <div class="font-font1 text-xl md:text-5xl text-center m-10 md:p-10">Address from Academic Authorities</div>
-    <div class="flex w-[80%] ml-auto mr-auto md:flex-row flex-col my-8">
-        <div class=" grid place-items-centerfont-mono  mx-5 mb-12">
-            <div class="bg-gradient-to-bbg-gradient-to-b from-rose-100 to-teal-100 shadow-2xl h-full   rounded-md">
-              <div class="flex justify-center items-center leading-none">
-                <img
-                  src="/t2.jpg"
-                  alt="pic"
-                  class="object-cover h-40 w-56 rounded-md shadow-2xl mt-6 transform -translate-y-10 hover:-translate-y-4 transition duration-700"
-                />
-              </div>
-              <div class="p-3 md:w-[25vw]">
-                <p class="font-thin">~Chairman</p>
-                <p class="block mb-1  text-2xl text-indigo-900">Path to Success: Learn, Adapt, and Work with Passion and Integrity.</p>
-                <p class="text-xs tracking-tighter text-gray-600  ">
-                  "Embrace continuous learning, cultivate resilience, 
-                  and work with passion and integrity on your path to success."
-                </p>
-              </div>
-             
-            </div>
-          </div>
-    
-          <div class=" grid place-items-centerfont-mono mb-12 mx-5">
-            <div class="bg-gradient-to-bbg-gradient-to-b from-rose-100 to-teal-100 shadow-2xl h-full   rounded-md">
-              <div class="flex justify-center items-center leading-none">
-                <img
-                  src="/t1.jpg"
-                  alt="pic"
-                  class="object-cover h-40 w-56 rounded-md shadow-2xl mt-6 transform -translate-y-10 hover:-translate-y-4 transition duration-700"
-                />
-              </div>
-              <div class="p-3 md:w-[25vw] ">
-                <p class="font-thin">~Principal</p>
-                <p class="block mb-1  text-2xl text-indigo-900">Embracing Your Passion: Igniting a Path to  Remarkable Future.</p>
-                <p class="text-xs tracking-tighter text-gray-600">
-                  "Let your passion ignite the path to a remarkable and rewarding future; stay true to yourself."
-                </p>
-              </div>
-             
-            </div>
-          </div>
-    
-          <div class=" grid place-items-centerfont-mono mb-12 mx-5">
-            <div class="bg-gradient-to-bbg-gradient-to-b from-rose-100 to-teal-100 shadow-2xl h-full   rounded-md">
-              <div class="flex justify-center items-center leading-none">
-                <img
-                  src="/t5.jpg"
-                  alt="pic"
-                  class="object-cover h-40 w-56 rounded-md shadow-2xl mt-6 transform -translate-y-10 hover:-translate-y-4 transition duration-700"
-                />
-              </div>
-              <div class="p-3 md:w-[25vw]">
-                <p class="font-thin">~Training & Placement Officer</p>
-                <p class="block mb-1  text-2xl text-indigo-900">Driving Greatness: Self-Motivation, Knowledge, and Resilience.</p>
-                <p class="text-xs tracking-tighter text-gray-600">
-                  "Self-motivation and a hunger for knowledge lead the way to greatness; overcome setbacks with resilience."
-                  
-                </p>
-              </div>
-             
-            </div>
-          </div>
-    </div>
+	<div class="font-font1 text-xl md:text-5xl text-center m-10 md:p-10">
+		Address from Academic Authorities
+	</div>
+	<div class="flex w-[80%] ml-auto mr-auto md:flex-row flex-col my-8">
+		<div class=" grid place-items-centerfont-mono  mx-5 mb-12">
+			<div
+				class="bg-gradient-to-bbg-gradient-to-b from-rose-100 to-teal-100 shadow-2xl h-full   rounded-md"
+			>
+				<div class="flex justify-center items-center leading-none">
+					<img
+						src="/t2.jpg"
+						alt="pic"
+						class="object-cover h-40 w-56 rounded-md shadow-2xl mt-6 transform -translate-y-10 hover:-translate-y-4 transition duration-700"
+					/>
+				</div>
+				<div class="p-3 md:w-[25vw]">
+					<p class="font-thin">~Chairman</p>
+					<p class="block mb-1  text-2xl text-indigo-900">
+						Path to Success: Learn, Adapt, and Work with Passion and Integrity.
+					</p>
+					<p class="text-xs tracking-tighter text-gray-600  ">
+						"Embrace continuous learning, cultivate resilience, and work with passion and integrity
+						on your path to success."
+					</p>
+				</div>
+			</div>
+		</div>
+
+		<div class=" grid place-items-centerfont-mono mb-12 mx-5">
+			<div
+				class="bg-gradient-to-bbg-gradient-to-b from-rose-100 to-teal-100 shadow-2xl h-full   rounded-md"
+			>
+				<div class="flex justify-center items-center leading-none">
+					<img
+						src="/principal-2.jpg"
+						alt="pic"
+						class="object-cover h-40 w-56 rounded-md shadow-2xl mt-6 transform -translate-y-10 hover:-translate-y-4 transition duration-700"
+					/>
+				</div>
+				<div class="p-3 md:w-[25vw] ">
+					<p class="font-thin">~Principal</p>
+					<p class="block mb-1  text-2xl text-indigo-900">
+						Embracing Your Passion: Igniting a Path to Remarkable Future.
+					</p>
+					<p class="text-xs tracking-tighter text-gray-600">
+						"Let your passion ignite the path to a remarkable and rewarding future; stay
+						true to yourself."
+					</p>
+				</div>
+			</div>
+		</div>
+
+		<div class=" grid place-items-centerfont-mono mb-12 mx-5">
+			<div
+				class="bg-gradient-to-bbg-gradient-to-b from-rose-100 to-teal-100 shadow-2xl h-full   rounded-md"
+			>
+				<div class="flex justify-center items-center leading-none">
+					<img
+						src="/t5.jpg"
+						alt="pic"
+						class="object-cover h-40 w-56 rounded-md shadow-2xl mt-6 transform -translate-y-10 hover:-translate-y-4 transition duration-700"
+					/>
+				</div>
+				<div class="p-3 md:w-[25vw]">
+					<p class="font-thin">~Training & Placement Officer</p>
+					<p class="block mb-1  text-2xl text-indigo-900">
+						Driving Greatness: Self-Motivation, Knowledge, and Resilience.
+					</p>
+					<p class="text-xs tracking-tighter text-gray-600">
+						"Self-motivation and a hunger for knowledge lead the way to greatness; overcome setbacks
+						with resilience."
+					</p>
+				</div>
+			</div>
+		</div>
+	</div>
 </section>
