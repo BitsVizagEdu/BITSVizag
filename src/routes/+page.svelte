10a98c42e8c7c57d01e1e66fb3462233632777b6
Rahul1342002 <trahul13402@gmail.com>
2024-04-09 17:07:33 +0530

    nav fixed

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,68 +1,57 @@
 <script>
-    import Events from "$lib/components/events.svelte"
-    import Strength from "$lib/components/strength.svelte"
-    import Committies from "$lib/components/committies.svelte"
-   
-    import Gallery from "$lib/components/gallery.svelte"
-    import Placement from "$lib/components/placement.svelte"
-    import {  showNavBar, toggleNavBar } from '$lib/stores/store.js';
-    import Video from "$lib/components/video.svelte"
-   
+	import Events from '$lib/components/events.svelte';
+	import Strength from '$lib/components/strength.svelte';
+	import Committies from '$lib/components/committies.svelte';
 
-    import {onMount} from "svelte";
-	import Placementheader from "$lib/components/placementheader.svelte";
-	import Notification from "$lib/components/notification.svelte";
-	
+	import Gallery from '$lib/components/gallery.svelte';
+	import Placement from '$lib/components/placement.svelte';
+	import { showNavBar, toggleNavBar } from '$lib/stores/store.js';
+	import Video from '$lib/components/video.svelte';
 
-    function scaleLogo(y){
-        if(typeof(y) !== "undefined"){
-            document.getElementById("scale-logo").style.transform = `scale(${1+y/700})`
-            document.getElementById("scale-logo").style.opacity = `${1-y/900}`
-        }
-    }
+	import { onMount } from 'svelte';
+	import Placementheader from '$lib/components/placementheader.svelte';
+	import Notification from '$lib/components/notification.svelte';
 
-    onMount(()=>{
+	function scaleLogo(y) {
+		if (typeof y !== 'undefined') {
+			document.getElementById('scale-logo').style.transform = `scale(${1 + y / 700})`;
+			document.getElementById('scale-logo').style.opacity = `${1 - y / 900}`;
+		}
+	}
 
-    })
+	onMount(() => {});
 
-    let y
-    $: scaleLogo(y)
-
-
+	let y;
+	$: scaleLogo(y);
 </script>
 
 <svelte:head>
-    <title>Home</title>
+	<title>Home</title>
 </svelte:head>
 
-<svelte:window bind:scrollY={y}/>
+<svelte:window bind:scrollY={y} />
 {#if !$showNavBar}
-    
-    <Notification/>
-    <div id="overlay" class="overlay overflow-hidden flex relative z-[-1] min-h-[75vh]">
-        <video playsinline autoplay muted loop>
-            <source src="/baba.webm" type="video/webm" />
-        </video>
+	<Notification />
+	<div id="overlay" class="overlay overflow-hidden flex relative z-[-1] min-h-[75vh]">
+		<video playsinline autoplay muted loop>
+			<source src="/baba.webm" type="video/webm" />
+		</video>
 
+		<div id="scale-logo" class="vidlogo absolute flex justify-center items-center h-full w-full">
+			<img src="/logow.svg" class="mb-24 w-auto lg:h-52 md:h-44 mr-4 pb-10 3xs:h-24 xs:h-32" />
+			<img src="/sq-1.svg" class=" w-auto mb-24 lg:h-60 h-72 md:h-44 3xs:h-24 xs:h-32" />
+		</div>
+	</div>
+	<Placementheader />
 
+	<h1 class="3xs:text-lg xs:text-xl md:text-4xl text-center p-4 md:p-10 bg-white ">
+		CELEBRATE LIFE AT BITS VIZAG
+	</h1>
 
-        <div id="scale-logo" class="vidlogo absolute flex justify-center items-center h-full w-full">
-            <img src="/logow.svg" class="mb-24 w-auto lg:h-52 md:h-44 mr-4 pb-10 3xs:h-24 xs:h-32" />
-            <img src="/sq-1.svg" class=" w-auto mb-24 lg:h-60 h-72 md:h-44 3xs:h-24 xs:h-32" />
-        </div>
-    </div>
-    <Placementheader/>
-    
-    <h1 class="3xs:text-lg xs:text-xl md:text-4xl text-center p-4 md:p-10 bg-white ">CELEBRATE LIFE AT BITS VIZAG</h1>
+	<Gallery />
+	<Events />
+	<Strength />
 
-    <Gallery/>
-    <Events/>
-    <Strength/>
-    
-    <Placement/>
-    <Committies/>
- 
+	<Placement />
+	<Committies />
 {/if}
-
-
-
