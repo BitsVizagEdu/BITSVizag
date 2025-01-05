fc4453bec1d1ad0594096db3c3405f8902557c5f
Tamada Rahul <trahul13402@gmail.com>
2025-01-06 01:28:33 +0530

    domain restored

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -11,38 +11,37 @@
     import { showNavBar, toggleNavBar } from '$lib/stores/store.js';
     import Footer from "$lib/components/footer.svelte";
   
-    let globalVar = false; // Default value
-    let isLoading = true;  // To show a loading state until the fetch completes
+    let globalVar = false; 
+    let isLoading = true;  
   
     onMount(async () => {
       try {
-        // Fetch the data directly in the component
+        
         const response = await fetch("https://test-delta-snowy.vercel.app/get-me");
         const data = await response.json();
   
-        // Set globalVar with the fetched value
+        
         globalVar = data.value;
       } catch (error) {
         console.error("Failed to fetch global variable:", error);
-        globalVar = false; // Default to false in case of error
-      } finally {
-        isLoading = false;  // End the loading state once fetching is complete
+        globalVar = false; 
+        isLoading = false;  
       }
     });
   
     onMount(() => {
-      AOS.init(); // Initialize AOS
+      AOS.init(); 
     });
   </script>
   
   <style>
     .spinner {
-      border: 4px solid #f3f3f3; /* Light grey */
-      border-top: 4px solid #3498db; /* Blue */
+      border: 4px solid #f3f3f3; 
+      border-top: 4px solid #3498db; 
       border-radius: 50%;
       width: 40px;
       height: 40px;
-      animation: spin 1.5s linear infinite; /* Continuous spinning */
+      animation: spin 1.5s linear infinite; 
       margin: 0 auto;
     }
   
@@ -61,12 +60,12 @@
   
   <main>
     {#if isLoading}
-      <!-- Display the loading spinner while data is being fetched -->
+      
       <div class="loading-container">
         <div class="spinner"></div>
       </div>
     {:else if globalVar} 
-      <!-- If globalVar is true -->
+      
       {#if $showNavBar}
       {:else}
         <Hero showNavBar={showNavBar}/>
@@ -77,10 +76,10 @@
         <Footer/>
       {/if}
     {:else}  
-      <!-- If globalVar is false -->
+      
       <div class="loading-container">
         <div class="spinner"></div>
-      </div>  <!-- Empty page -->
+      </div>  
     {/if}
   </main>
   
